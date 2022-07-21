1cad984df4e09177443bb0b1fa07b15704d802fc
duaaaryan <69601322+duaaaryan@users.noreply.github.com>
2022-07-21 12:23:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/u7xQe0z9WU-Yy484/scene.splinecode" />
+  );
+}
